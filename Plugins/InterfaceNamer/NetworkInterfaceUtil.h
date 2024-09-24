696e26776c5dfa064df36ec2cf62cc3d08f67c86
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2024-09-24 17:44:43 +0000

    configd-1345
    Imported from configd-1345.tar.gz

--- Plugins/InterfaceNamer/NetworkInterfaceUtil.h
+++ Plugins/InterfaceNamer/NetworkInterfaceUtil.h
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2006-2023 Apple Inc. All rights reserved.
+ * Copyright (c) 2023 Apple Inc.  All Rights Reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -17,19 +17,32 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
-#ifndef _SET_HOSTNAME_H
-#define _SET_HOSTNAME_H
+/*
+ * NetworkInterfaceUtil.h
+ * - access to reserved network interface unit numbers
+ */
 
-#include <dispatch/dispatch.h>
+#ifndef	_NETWORK_INTERFACE_UTIL_H
+#define	_NETWORK_INTERFACE_UTIL_H
 
-__BEGIN_DECLS
+#include <SystemConfiguration/SCNetworkConfiguration.h>
 
-void	load_hostname(dispatch_queue_t queue);
+typedef unsigned int	IFUnit;
 
-__END_DECLS
+typedef struct {
+	IFUnit	start;
+	IFUnit	end;
+} IFUnitRange, *IFUnitRangeRef;
 
-#endif /* _SET_HOSTNAME_H */
+IFUnit
+NetworkInterfacePrefixGetReservedUnits(CFStringRef prefix);
+
+Boolean
+NetworkInterfaceGetReservedRange(SCNetworkInterfaceRef netif,
+				 IFUnitRangeRef unit_range);
+
+#endif /* _NETWORK_INTERFACE_UTIL_H */
